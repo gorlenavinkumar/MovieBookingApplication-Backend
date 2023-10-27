755a15fd6379b1144c1dc5df4b8db7fc10a73122
navinkumar <navinkumar.gorle@gmail.com>
2023-10-27 20:17:29 +0530

    Updated tests

--- src/test/java/com/rbp/bookmymovie/MovieappApplicationTests.java
+++ src/test/java/com/rbp/bookmymovie/MovieappApplicationTests.java
@@ -1,13 +1,12 @@
-package com.rbp.bookmymovie;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MovieappApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.rbp.bookmymovie;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class MovieappApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
